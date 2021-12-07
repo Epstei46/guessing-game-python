0d112cbf5eb970b7883adcfae8d3e7cffd2b2dc9
Steven Epstein <sbepstein42@gmail.com>
2021-12-07 18:36:06 -0500

    imported random to get and save a random number

--- game.py
+++ game.py
@@ -1,6 +1,12 @@
 """A number-guessing game."""
 
-print("Greetings Player!")
+import random
+# from random import function_name
+
+print("Greetings Player! Enter below your preferred name:")
 
 name = input()
 # print(name)
+
+random_num = random.randint(1,100)
+# print(num)
